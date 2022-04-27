60774d07541090f89da33eef1598568bf7540479
jvega190 <josevega19@gmail.com>
2022-04-27 09:13:28 -0600

    Blueprint updates (#60)
    * Don't show Launcher and logo in Studio's search tool #5510
    
    * Update version
    
    * Remove the edit button from the RTE toolbar #5526
    
    * Update version
    
    * Update craftercms.guest to craftercms.xb

--- static-assets/js/custom.js
+++ static-assets/js/custom.js
@@ -17,7 +17,7 @@
 
   function getICE(model, fieldId, index) {
 
-    const iceAttrs = craftercms.guest.getICEAttributes({
+    const iceAttrs = craftercms.xb.getICEAttributes({
       model,
       isAuthoring: isAuthoring(),
       ...(
@@ -193,12 +193,12 @@
 
       });
 
-      craftercms.guest?.contentController?.operations$
+      craftercms.xb?.contentController?.operations$
         .pipe(
           operators.filter(op => op.type === 'INSERT_COMPONENT_OPERATION' && op.args.contentType.id === '/component/component-quote-item')
         )
         .subscribe((op) => {
-          craftercms.guest.fromTopic('INSERT_OPERATION_COMPLETE')
+          craftercms.xb.fromTopic('INSERT_OPERATION_COMPLETE')
             .pipe(
               operators.filter(({payload}) => payload.modelId === op.args.modelId)
             )
